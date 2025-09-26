ef250d15c80850e5c5ba59f40a81c078b77f53ee
Max1ev <kapas.max@gmail.com>
2025-09-26 18:05:35 +0300

    js-hw-11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://pixabay.com/api/',
+  timeout: 12000,
+});
+
+// ⚠️ свій ключ підстав
+const API_KEY = '52480069-e3f81e86b58f6705753339629';
+
+export function getImagesByQuery(query) {
+  return api
+    .get('', {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 20,
+      },
+    })
+    .then(res => res.data);
+}
---
+++ src/main.js
@@ -0,0 +1,144 @@
+
+import './css/styles.css';
+import './css/loader.css';
+
+
+import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.css';
+
+
+const api = axios.create({
+  baseURL: 'https://pixabay.com/api/',
+  timeout: 12000,
+});
+
+
+const API_KEY = '52480069-e3f81e86b58f6705753339629';
+
+
+const form = document.getElementById('search-form');
+const input = document.getElementById('search-text');
+const gallery = document.getElementById('gallery');
+const loaderOverlay = document.getElementById('loader');
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
+function showLoader() {
+  loaderOverlay.classList.remove('is-hidden');
+}
+function hideLoader() {
+  loaderOverlay.classList.add('is-hidden');
+}
+function clearGallery() {
+  gallery.innerHTML = '';
+}
+function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="card">
+        <a href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+        </a>
+        <div class="meta">
+          <div><b>Likes</b>${likes}</div>
+          <div><b>Views</b>${views}</div>
+          <div><b>Comments</b>${comments}</div>
+          <div><b>Downloads</b>${downloads}</div>
+        </div>
+      </li>`
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+
+function getImagesByQuery(query) {
+  return api
+    .get('', {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 20,
+      },
+    })
+    .then(res => res.data);
+}
+
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const query = input.value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      title: 'Oops',
+      message: 'Please type something to search.',
+      position: 'topRight',
+      class: 'iziToast-color-yellow',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      const hits = data.hits;
+      if (!hits || hits.length === 0) {
+        iziToast.error({
+          title: 'No results',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+          class: 'iziToast-color-red',
+        });
+        return;
+      }
+      createGallery(hits);
+
+      iziToast.success({
+        title: 'Success',
+        message: `Found ${hits.length} images`,
+        position: 'topRight',
+        class: 'iziToast-color-green',
+      });
+    })
+    .catch(err => {
+      iziToast.error({
+        title: 'Error',
+        message: 'Something went wrong. Please try again later.',
+        position: 'topRight',
+        class: 'iziToast-color-red',
+      });
+      console.error(err);
+    })
+    .finally(() => {
+      hideLoader();
+    });
+}
